251fccb111c67687e012a12439b13adf412954e3
Sercan Altundaş <sercan.altundas@gmail.com>
2017-12-02 19:03:34 +0200

    cage model added and basic functionality scripted

---
+++ Assets/Scripts/Cage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Cage : MonoBehaviour {
+    public int life;                    //hp of cage
+    public GameObject damaged;
+    public GameObject undamaged;
+    public Transform exitPoint;         //where the prisoner will walk
+    public ParticleSystem explosion;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        
+        life -= Random.Range(5, 25);
+
+        if(life <= 0)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity, transform);
+            undamaged.SetActive(false);
+            damaged.SetActive(true);
+        }
+    }
+}
