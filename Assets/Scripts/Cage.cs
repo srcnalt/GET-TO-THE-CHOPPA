92d3e324ead0c48dd0e8e2327dcd5b63c25e2a3e
Girts Kesteris <girts.kesteris@riga.orangelv.com>
2017-12-02 21:07:30 +0200

    added nicholas cage

--- Assets/Scripts/Cage.cs
+++ Assets/Scripts/Cage.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
 
-public class Cage : MonoBehaviour {
-    public int life;                    //hp of cage
-    public GameObject damaged;
-    public GameObject undamaged;
-    public Transform exitPoint;         //where the prisoner will walk
-    public ParticleSystem explosion;
+public class Cage : MonoBehaviour
+{
+    [SerializeField]
+    private int life;
 
+    [SerializeField]//hp of cage
+    private GameObject damaged;
+
+    [SerializeField]
+    private GameObject undamaged;
+
+    [SerializeField]
+    private Transform exitPoint;
+
+    [SerializeField]//where the prisoner will walk
+    private ParticleSystem explosion;
+
+    [SerializeField]
+    private Nicholas nicholas;
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(collision.gameObject);
-        
+
         life -= Random.Range(5, 25);
 
-        if(life <= 0)
+        if (life <= 0)
         {
             Instantiate(explosion, transform.position, Quaternion.identity, transform);
             undamaged.SetActive(false);
             damaged.SetActive(true);
+
+            nicholas.Release();
         }
     }
+
 }
