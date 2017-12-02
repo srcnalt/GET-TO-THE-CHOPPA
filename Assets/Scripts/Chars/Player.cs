be6885c5b6c14da80774f51e8ea0741e2e08ef2f
Sacristan <girts.kesteris@gmail.com>
2017-12-03 00:12:13 +0200

    correct yaw controls

--- Assets/Scripts/Chars/Player.cs
+++ Assets/Scripts/Chars/Player.cs
@@ -10,16 +10,6 @@ public class Player : Character, Character.IDamageable
     public float Pitch { get { return pitch; } }
     public bool IsAiming { get; private set; }
 
-    public void ApplyDamage(float dmg)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void Die()
-    {
-        throw new System.NotImplementedException();
-    }
-
     protected override void Update()
     {
         PlayerControls();
@@ -44,7 +34,17 @@ public class Player : Character, Character.IDamageable
         pitch += (-Input.GetAxisRaw("Mouse Y") * GameSettings.MOUSE_SENSITIVITY);
         pitch = Mathf.Clamp(pitch, -85f, +85f);
 
-        transform.Rotate(Vector3.up * yaw * Time.deltaTime);
+        //transform.Rotate(Vector3.up * yaw * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(0, yaw, 0);
         velocity += transform.localRotation * new Vector3(inputVec.x, 0, inputVec.y) * maxSpeed * Time.deltaTime;
+    }
+    public void ApplyDamage(float dmg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die()
+    {
+        throw new System.NotImplementedException();
     }
 }
