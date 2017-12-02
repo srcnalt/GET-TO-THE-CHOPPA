85554f6c5143ba13650c4b6e69bca61b0285b04e
Sercan Altundaş <sercan.altundas@gmail.com>
2017-12-02 18:13:23 +0200

    exploding coconuts and palm trees added

---
+++ Assets/Scripts/Coconut.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coconut : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private float farArea = 2;
+    private float farDamageMultiplier = 75;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity, transform);
+
+        Invoke("DestroyCoconut", 2);
+
+        Collider[] colls = Physics.OverlapSphere(transform.position, farArea);
+
+        foreach (Collider coll in colls)
+        {
+            if(coll.tag == "Enemy")
+            {
+                float distance = Vector3.Distance(coll.transform.position, transform.position);
+
+                float damage = 1 / distance * farDamageMultiplier;
+            }
+        }
+    }
+
+    private void DestroyCoconut()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PalmTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalmTree : MonoBehaviour
+{
+    public List<Rigidbody> coconuts;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Yo");
+
+        Destroy(collision.gameObject);
+
+        if (coconuts.Count > 0)
+        {
+            coconuts[0].isKinematic = false;
+            coconuts.RemoveAt(0);
+        }
+    }
+}
