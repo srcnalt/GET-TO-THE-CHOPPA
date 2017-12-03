370420a9ff2ba381b8b33fbd61a7bbe0f704b3fa
Sacristan <girts.kesteris@gmail.com>
2017-12-03 12:49:04 +0200

    added basic wandering pathfinding for nichlas

--- Assets/Scripts/Chars/Nicholas.cs
+++ Assets/Scripts/Chars/Nicholas.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class Nicholas : Character
 {
+    private const float CloseEnoughDistance = 0.5f;
+
     public enum State
     {
         None,
@@ -28,12 +32,14 @@ public class Nicholas : Character
 
     State currentState = State.None;
 
+    private NavMeshAgent _navMeshAgent;
+
     public State CurrentState
     {
         get { return currentState; }
         private set
         {
-            if(currentState != value)
+            if (currentState != value)
             {
                 currentState = value;
                 OnStateChanged(currentState);
@@ -44,6 +50,7 @@ public class Nicholas : Character
     private void Start()
     {
         CurrentState = State.Captured;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     public void Release()
@@ -79,11 +86,13 @@ public class Nicholas : Character
     private void HandleReleased()
     {
         ChangeFacialExpression(released);
+        StartCoroutine(ReleasedRoutine());
     }
 
     private void HandleWandering()
     {
         ChangeFacialExpression(wandering);
+        StartCoroutine(WanderingRoutine());
     }
 
     private void HandleFollowing()
@@ -95,4 +104,47 @@ public class Nicholas : Character
     {
         facialExpressionRenderer.material.SetTexture("_MainTex", expressionTex);
     }
+
+    private IEnumerator ReleasedRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        _navMeshAgent.SetDestination(transform.position + transform.forward * 3f);
+        yield return new WaitForSeconds(3f);
+        CurrentState = State.Wandering;
+    }
+
+    private IEnumerator WanderingRoutine()
+    {
+        while (CurrentState == State.Wandering)
+        {
+            _navMeshAgent.isStopped = false;
+
+            Vector3 destinationPoint;
+            RandomPoint(transform.position, 10f, out destinationPoint);
+
+            _navMeshAgent.SetDestination(destinationPoint);
+
+            yield return new WaitUntil(() => Vector3.Distance(transform.position, destinationPoint) <= CloseEnoughDistance);
+            _navMeshAgent.isStopped = true;
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
 }
