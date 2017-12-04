e1b739b9a8e614661a5b7dbb1a6415f1bb279690
Sacristan <girts.kesteris@gmail.com>
2017-12-04 03:51:52 +0200

    added ahnold audio

---
+++ Assets/ArnoldAudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArnoldAudioController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] releasedCageAudio;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        GameManager.Instance.OnNicholasReleased += Instance_OnNicholasReleased;
+    }
+
+    private void Instance_OnNicholasReleased(Nicholas nicholasSaved)
+    {
+        _audioSource.PlayOneShot(GetRandomAudioClip(releasedCageAudio));
+    }
+
+    private AudioClip GetRandomAudioClip(AudioClip[] collection)
+    {
+        int index = Random.Range(0, collection.Length);
+        return collection[index];
+    }
+}
