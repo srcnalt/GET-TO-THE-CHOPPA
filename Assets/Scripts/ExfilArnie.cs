68a19dd8d5754ec2b2549ccdbee0b4cd7852ce44
Sercan Altundaş <sercan.altundas@gmail.com>
2017-12-04 16:28:35 +0200

    epilogue

--- Assets/Scripts/ExfilArnie.cs
+++ Assets/Scripts/ExfilArnie.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ExfilArnie : MonoBehaviour
 {
     private bool canSaveArnie = false;
+    private bool startEpilogue = false;
+
+    public Transform choppa;
+    public List<Transform> wayPoints;
+    public Transform camPosition;
+    public RectTransform panel;
+    public Image whiteScreen;
+    public GameObject explosion;
+    public AudioSource audioSource;
 
     private void Start()
     {
@@ -23,12 +34,60 @@ public class ExfilArnie : MonoBehaviour
         if (player != null)
         {
             if (canSaveArnie)
-                GameManager.Instance.HeroSaved();
+            {
+                startEpilogue = true;
+                panel.gameObject.SetActive(false);
+
+                GameManager.Instance.Player.gameObject.SetActive(false);
+                GameManager.Instance.PlayerCamera.GetComponent<PlayerCamera>().enabled = false;
+
+                Camera.main.transform.position = camPosition.position;
+
+                //GameManager.Instance.HeroSaved();
+            }
             else
                 GameUI.Instance.ShowNicholasNotSavedInfo();
 
         }
     }
 
+    private void Update()
+    {
+        if (startEpilogue)
+        {
+            Camera.main.transform.LookAt(choppa);
 
+            choppa.position = Vector3.MoveTowards(choppa.position, wayPoints[0].position, .3f);
+
+            if(wayPoints.Count > 0 && choppa.position == wayPoints[0].position)
+            {
+                wayPoints.RemoveAt(0);
+            }
+
+            if (wayPoints.Count == 0)
+            {
+                Instantiate(explosion, choppa.position, Quaternion.identity);
+                StartCoroutine(FadeToWhite());
+                startEpilogue = false;
+            }
+        }
+    }
+
+    IEnumerator FadeToWhite()
+    {
+        yield return new WaitForSeconds(0.5f);
+        choppa.gameObject.SetActive(false);
+
+        Color color = new Color(1, 1, 1, 0);
+
+        while (color.a < 1f)
+        {
+            audioSource.volume -= Time.deltaTime * 10;
+            color.a += Time.deltaTime * 1;
+            whiteScreen.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(2);
+    }
 }
