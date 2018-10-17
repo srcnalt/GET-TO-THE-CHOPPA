69c3d28d3874bffd9d9ba6520ed15c3d7cf8f198
Sacristan <girts.kesteris@gmail.com>
2018-10-17 10:06:28 +0300

    fix menu anim

---
+++ Assets/Scripts/MainMenuItemAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuItemAnimator : MonoBehaviour
+{
+    [SerializeField] private Vector2 startAnchorPos;
+    [SerializeField] private float timeInSeconds = 2f;
+    [SerializeField] private float delay = 0f;
+
+    private IEnumerator Start()
+    {
+        RectTransform _rectTransform = GetComponent<RectTransform>();
+        Vector2 targetPos = _rectTransform.anchoredPosition;
+
+        _rectTransform.anchoredPosition = startAnchorPos;
+        yield return new WaitForSeconds(delay);
+
+        float t = 0f;
+
+        while (t <= 1f)
+        {
+            t += Time.deltaTime / timeInSeconds;
+            _rectTransform.anchoredPosition = Vector2.Lerp(startAnchorPos, targetPos, t);
+            yield return null;
+        }
+
+    }
+
+
+
+}
